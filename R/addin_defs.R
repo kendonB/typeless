e1ed4ee2155954725164b165017385788b17a228
Kendon Bell <kmb56@berkeley.edu>
2017-04-19 16:31:55 -0700

    first commit

---
+++ R/addin_defs.R
@@ -0,0 +1,157 @@
+#' Insert dplyr::
+#'
+#' Call this function as an addin to insert \code{dplyr::} at the cursor position.
+#'
+#' @export
+insertDplyrAddin <- function() {
+  rstudioapi::insertText("dplyr::")
+}
+
+#' Insert tidyr::.
+#'
+#' Call this function as an addin to insert \code{tidyr::} at the cursor position.
+#'
+#' @export
+insertTidyrAddin <- function() {
+  rstudioapi::insertText("tidyr::")
+}
+
+
+#' Insert purrr::.
+#'
+#' Call this function as an addin to insert \code{purrr::} at the cursor position.
+#'
+#' @export
+insertPurrrAddin <- function() {
+  rstudioapi::insertText("purrr::")
+}
+
+
+#' Insert mutate.
+#'
+#' Call this function as an addin to insert \code{mutate(} at the cursor position.
+#'
+#' @export
+insertMutateAddin <- function() {
+  rstudioapi::insertText("mutate(")
+}
+
+
+#' Insert select
+#'
+#' Call this function as an addin to insert \code{select(} at the cursor position.
+#'
+#' @export
+insertSelectAddin <- function() {
+  rstudioapi::insertText("select(")
+}
+
+#' Insert filter.
+#'
+#' Call this function as an addin to insert \code{filter(} at the cursor position.
+#'
+#' @export
+insertFilterAddin <- function() {
+  rstudioapi::insertText("filter(")
+}
+
+#' Insert as_tibble
+#'
+#' Call this function as an addin to insert \code{as_tibble(} at the cursor position.
+#'
+#' @export
+insertAsTibbleAddin <- function() {
+  rstudioapi::insertText("as_tibble(")
+}
+
+#' Insert lubridate::
+#'
+#' Call this function as an addin to insert \code{lubridate::} at the cursor position.
+#'
+#' @export
+insertLubridateAddin <- function() {
+  rstudioapi::insertText("lubridate::")
+}
+
+#' Insert stringr::
+#'
+#' Call this function as an addin to insert \code{stringr::str_} at the cursor position.
+#'
+#' @export
+insertStringrAddin <- function() {
+  rstudioapi::insertText("stringr::str_")
+}
+
+#' Insert group_by
+#'
+#' Call this function as an addin to insert \code{group_by(} at the cursor position.
+#'
+#' @export
+insertGroupByAddin <- function() {
+  rstudioapi::insertText("group_by(")
+}
+
+#' Insert summarize()
+#'
+#' Call this function as an addin to insert \code{summarize(} at the cursor position.
+#'
+#' @export
+insertSummarizeAddin <- function() {
+  rstudioapi::insertText("summarize(")
+}
+
+#' Insert nest
+#'
+#' Call this function as an addin to insert \code{nest(} at the cursor position.
+#'
+#' @export
+insertNestAddin <- function() {
+  rstudioapi::insertText("nest(")
+}
+
+#' Insert unnest.
+#'
+#' Call this function as an addin to insert \code{unnest(} at the cursor position.
+#'
+#' @export
+insertUnnestAddin <- function() {
+  rstudioapi::insertText("unnest(")
+}
+
+#' Insert spread.
+#'
+#' Call this function as an addin to insert \code{spread(} at the cursor position.
+#'
+#' @export
+insertSpreadAddin <- function() {
+  rstudioapi::insertText("spread(")
+}
+
+#' Insert gather.
+#'
+#' Call this function as an addin to insert \code{gather(} at the cursor position.
+#'
+#' @export
+insertGatherAddin <- function() {
+  rstudioapi::insertText("gather(")
+}
+
+#' Insert map.
+#'
+#' Call this function as an addin to insert \code{map(} at the cursor position.
+#'
+#' @export
+insertMapAddin <- function() {
+  rstudioapi::insertText("map(")
+}
+
+
+#' Insert left_join.
+#'
+#' Call this function as an addin to insert \code{left_join(} at the cursor position.
+#'
+#' @export
+insertLeftJoinAddin <- function() {
+  rstudioapi::insertText("left_join(")
+}
+
